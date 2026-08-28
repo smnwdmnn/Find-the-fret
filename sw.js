0aa16e83d13d53600d42a270ff4c0945f8537dbe
smnwdmnn <44921184+smnwdmnn@users.noreply.github.com>
2026-08-28 19:33:31 +0200

    Add files via upload
    Only naturals round

--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-const CACHE="fret-speed-v11";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg?v=11"];
-self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+const CACHE="fret-speed-v12";const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg?v=12"];self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
