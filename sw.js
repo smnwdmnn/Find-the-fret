3d780f946ddf9ccf727674397428d1a464def521
smnwdmnn <44921184+smnwdmnn@users.noreply.github.com>
2026-08-31 15:56:10 +0200

    Add files via upload
    ItI

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="fret-speed-v22";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg?v=22"];
+const CACHE="fret-speed-v24";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg?v=24"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
